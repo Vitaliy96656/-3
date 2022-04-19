070e083de7fa43e4adf788c9580bfb26be593727
Vitaliy96656 <77229631+Vitaliy96656@users.noreply.github.com>
2022-04-19 21:59:52 +0500

    Add files via upload

---
+++ lesson 3 task 1.py
@@ -0,0 +1,11 @@
+
+#Списки
+list.append(x), list.reverse(), list.remove(x), list.insert(i, x), list.sort([key=функция])
+#Строки
+len(S), S.split(символ), S.upper(), S.lower(), S.replace(шаблон, замена[, maxcount])
+#словари
+dict.keys(), dict.items(), dict.values(), dict.update([other]), dict.clear()
+#множества
+len(s), set.add(elem), set.remove(elem), set.discard(elem), set.pop(), 
+
+
